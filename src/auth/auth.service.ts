b461a2937aa72acc41294df4d8503e375afc69c3
Ian Ndegwa <ndegwaian241@gmail.com>
2024-12-09 14:47:18 -0800

    messahe

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,4 +2,17 @@ import { Injectable } from "@nestjs/common";
 
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    static signup() {
+        throw new Error("Method not implemented.");
+    }
+
+
+    signup() {
+        return { msg: 'I have signed up' };
+    }
+
+    signin() {
+        return { msg: 'I have signed in' };
+    }
+}
