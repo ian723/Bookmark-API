cbdeb9724d68f6e986ebd73ce835379faa128c97
Ian Ndegwa <ndegwaian241@gmail.com>
2024-12-10 00:45:21 +0300

    message

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,25 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "../prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) { }
-    signup() {
-        return { msg: 'I have signed up' };
-    }
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    /* generate the password hash */
+    const hash = await argon.hash(dto.password);
 
-    signin() {
-        return { msg: 'I have signed in' };
-    }
+    /* save the user in the database */
+    const user = await this.prisma.user.create({
+      data: { email: dto.email, hash },
+    });
+
+    /* return the saved user */
+    return user;
+  }
+
+  signin() {
+    return { msg: 'I have signed in' };
+  }
 }
