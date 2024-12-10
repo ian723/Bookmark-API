8ac27bd5008e3b49ab01f9f146e63716e3c2d529
Ian Ndegwa <ndegwaian241@gmail.com>
2024-12-10 03:30:38 +0300

    message

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -10,16 +14,65 @@ export class AuthService {
     /* generate the password hash */
     const hash = await argon.hash(dto.password);
 
-    /* save the user in the database */
-    const user = await this.prisma.user.create({
-      data: { email: dto.email, hash },
-    });
+    try {
+      /* save the user in the database */
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    /* return the saved user */
-    return user;
+      /* delete the password hash */
+      delete user.hash;
+
+      /* return the saved user */
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
+      }
+
+      throw error;
+    }
   }
+  async signin(dto: AuthDto) {
+    /* find the user by email */
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+    /* if user doesn't exist throw exception */
+    if (!user)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+    /* compare password */
 
-  signin() {
-    return { msg: 'I have signed in' };
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+
+    /* if password incorrect throw exception */
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+
+    /* delete the password hash */
+    delete user.hash;
+
+    /* return the user */
+    return user;
   }
 }
