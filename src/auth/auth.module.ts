1dc59ec37ea322588e272f1af610330ba9d72da9
Ian Ndegwa <ndegwaian241@gmail.com>
2024-12-11 23:29:01 +0300

    message

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,12 @@
-import { Module } from "@nestjs/common";
-import { JwtModule } from "@nestjs/jwt";
-import { JwtStrategy } from "./strategy";
-import { AuthService } from "./auth.service";
-import { AuthController } from "./auth.controller";
-
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
-    imports: [JwtModule.register({})],
-    controllers: [AuthController],
-    providers: [AuthService, JwtStrategy],
+  imports: [JwtModule.register({})],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
 })
-
 export class AuthModule {}
-
-
